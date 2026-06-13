4e322600a55e3c6d082b58809f764817d7034ad3
Mac <akinkuotutobi6@gmail.com>
2026-06-13 11:44:29 +0100

    resolved errors

--- next.config.ts
+++ next.config.ts
@@ -3,10 +3,8 @@ import path from 'path';
 
 const nextConfig: NextConfig = {
   outputFileTracingRoot: path.join(__dirname),
-  experimental: {
-    turbo: {
-      root: path.join(__dirname),
-    },
+  turbopack: {
+    root: path.join(__dirname),
   },
   images: {
     remotePatterns: [
